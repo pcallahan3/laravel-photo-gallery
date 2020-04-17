c022052b2e28c8a2ced6a3630f6fb35e3846cf52
Patrick <you@example.com>
2020-04-17 12:51:49 -0700

    Created first view

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+
+  <link rel="stylesheet"  href="/css/master.css">
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+</head
+<html>
+<title>Photo Gallery</title>
+<body>
+
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
