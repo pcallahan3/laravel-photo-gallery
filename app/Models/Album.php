55d0e25ac5379b3c11a3846eeec7fd0e8d99c832
Patrick <you@example.com>
2020-04-16 22:11:46 -0700

    Added index() and create() in Albums model and named routes

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
-    //
+    public function index(){
+      return view('albums.index');
+    }
+
+    public function create(){
+      return view('albums.create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AlbumsController@index');
+Route::get('/albums', 'AlbumsController@index');
+Route::get('/albums/create', 'AlbumsController@create');
+Route::get('/albums/store', 'AlbumsController@store');
